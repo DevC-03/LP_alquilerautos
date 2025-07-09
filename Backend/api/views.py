baff66a6b107fe0e06ae3e0e47b0ac1df09654a0
C <carlosestb11@gmail.com>
2025-07-09 13:35:35 -0500

    v.1.5

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -68,13 +68,15 @@ class Login(ObtainAuthToken):
             'username': user.username
         })
 
-class Login(ObtainAuthToken):
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key})
-        else:
-            return Response({"error": "Credenciales Inválidas"}, status=400)
+# class Login(ObtainAuthToken):
+#     def post(self, request):
+#         username = request.data.get("username")
+#         password = request.data.get("password")
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             token, _ = Token.objects.get_or_create(user=user)
+#             return Response({"token": token.key})
+#         else:
+#             return Response({"error": "Credenciales Inválidas"}, status=400)
+
+
